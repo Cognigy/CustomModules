6c89f3efcb96c540e0a2f5e05e274dc982a7a3ef
teusz.alexander@gmail.com <teusz.alexander@gmail.com>
2019-03-18 16:58:28 +0100

    changed language var name

--- modules/cognigy-datepicker/src/module.ts
+++ modules/cognigy-datepicker/src/module.ts
@@ -1,6 +1,7 @@
 /**
  * Use the Cognigy Datepicker
  * @arg {CognigyScript} `eventName` The event for the date
+ * @arg {Select[ar,at,az,be,bg,bn,catcs,cy,da,de,en,eo,es,et,fa,fi,fo,fr,gr,he,hi,hr,hu,id,is,it,ja,ko,km,kz,lt,lv,mk,mn,ms,my,nl,no,pa,pl,pt,ro,ru,si,sk,sl,sq,sr,sv,th,tr,uk,vn,zh]} `language` The language of the datepicker
  * @arg {CognigyScript} `minDate` The minimum date the user can choose
  * @arg {CognigyScript} `maxDate` The maximum date the user can choose
  * @arg {Boolean} `disableRange` If you want to disable a range in the next option
@@ -8,7 +9,7 @@
  * @arg {Boolean} `enableTime` If you want the user to pick the time
  * @arg {Select[single,multiple,range]} `mode` If the user can choose one or more dates
  */
-async function datepicker(input: IFlowInput, args: { eventName: string, minDate: string, maxDate: string, disableRange: boolean, disable: [], enableTime: boolean, mode: string, stopOnError: boolean }) {
+async function datepicker(input: IFlowInput, args: { eventName: string, language: string, minDate: string, maxDate: string, disableRange: boolean, disable: [], enableTime: boolean, mode: string, stopOnError: boolean }) {
 	// check if necessary arguments are present
 	if (!args.eventName) return Promise.reject("Event not provided.");
 
@@ -18,6 +19,7 @@ async function datepicker(input: IFlowInput, args: { eventName: string, minDate:
 			type: "date-picker",
 			data: {
 				eventName: args.eventName,
+				locale: args.language,
 				enableTime: args.enableTime,
 				mode: args.mode,
 				disable: getDisabledDates(args.disableRange, args.disable),
