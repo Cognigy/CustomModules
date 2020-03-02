ad10fb90a188bb50c454f36905c9f394d97ff6c9
syla223 <61319399+syla223@users.noreply.github.com>
2020-03-02 17:08:52 +0100

    Add files via upload

---
+++ modules/sentiment/src/module.ts
@@ -0,0 +1,18 @@
+const Sentiment = require('sentiment');
+
+/**
+ * Gives sentiment of the text
+ */
+async function sentiment(input: IFlowInput, args: {}): Promise<IFlowInput | {}> {
+    
+    const sent = new Sentiment();
+    const text = input.input.text;
+    const result = sent.analyze(text);
+
+    return new Promise(resolve => {
+        input.actions.addToContext('sentiment', result, 'simple');
+        resolve(input);
+    });
+}
+
+module.exports.sentiment = sentiment
