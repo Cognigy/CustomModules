ca468c1b5a604a1c45e47f8a00d7e6d648fe03a0
syla223 <61319399+syla223@users.noreply.github.com>
2020-03-03 14:34:11 +0100

    Add files via upload

---
+++ modules/sentiment_multilingual/src/module.ts
@@ -0,0 +1,23 @@
+const {SentimentManager} = require('node-nlp');
+const {Language} = require('node-nlp');
+
+/**
+ * Gives sentiment of the text
+ */
+async function multi_sentiment(input: IFlowInput, args: {}): Promise<IFlowInput | {}> {
+
+    const language = new Language();
+    const sent = new SentimentManager();
+    const text = input.input.text;
+    const guess = language.guess(text,);
+    const lang = guess[0]['alpha2']
+
+    const result = await sent.process(lang,text);
+
+    return new Promise(resolve => {
+        input.actions.addToContext('sentiment', result, 'simple');
+        resolve(input);
+    });
+}
+
+module.exports.multi_sentiment = multi_sentiment
