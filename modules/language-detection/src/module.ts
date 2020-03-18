f4ca4cd1034f9543e29ede5010c20abed8f70745
syla223 <61319399+syla223@users.noreply.github.com>
2020-03-18 18:52:41 +0100

    Update module.ts

--- modules/language-detection/src/module.ts
+++ modules/language-detection/src/module.ts
@@ -2,15 +2,18 @@ const langdetect = require('langdetect');
 
 /**
  * Identifies the language of the written text
+ * @arg {CognigyScript} `store` Where to store the result
+ * @arg {Boolean} `writeToContext` Whether to write to Cognigy Context (true) or Input (false)
  */
-async function language(input: IFlowInput, args: {}): Promise<IFlowInput | {}> {
+async function language(input: IFlowInput, args: {store: string,  writeToContext: boolean}}): Promise<IFlowInput | {}> {
     
     const text = input.input.text;
     const result = langdetect.detect(text);
 
     return new Promise(resolve => {
-        input.actions.addToContext('language', result, 'simple');
-        resolve(input);
+        if (args.writeToContext) input.context.getFullContext()[args.store] = result;
+	    else input.input[args.store] = result;
+	    resolve(input);
     });
 }
 
