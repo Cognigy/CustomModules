228a4045ca26cc92920fac77d3f9f6c050d496ce
syla223 <61319399+syla223@users.noreply.github.com>
2020-03-18 18:47:06 +0100

    Update module.ts

--- modules/tensorFlow/src/module.ts
+++ modules/tensorFlow/src/module.ts
@@ -1,6 +1,8 @@
 const npmModel = require('@tensorflow-models/toxicity')
 /**
  * Template for usage of a TensorFlow.js module in a Custom Module
+ * @arg {CognigyScript} `store` Where to store the result
+ * @arg {Boolean} `writeToContext` Whether to write to Cognigy Context (true) or Input (false)
  */
 async function functionName(input: IFlowInput, args: {}): Promise<IFlowInput | {}> {
 	//load the model
@@ -11,9 +13,10 @@ async function functionName(input: IFlowInput, args: {}): Promise<IFlowInput | {
 	const result = await model.classify(text);
 
 	return new Promise(resolve => {
-		//write the result into the context
-        input.actions.addToContext('name', result, 'simple');
-        resolve(input);
+		//write the result to the Context or Input object
+        	if (args.writeToContext) input.context.getFullContext()[args.store] = result;
+	 	else input.input[args.store] = result;
+	        resolve(input);
     });
 }
 // You have to export the function, otherwise it is not available
