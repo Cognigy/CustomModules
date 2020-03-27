06a80ca0c5d7c096bf082c54e35ca631402083f3
Alexander Teusz <teusz.alexander@gmail.com>
2020-03-27 09:46:39 +0100

    refactored module code for sendSMS node

--- modules/twilio/src/module.ts
+++ modules/twilio/src/module.ts
@@ -6,33 +6,42 @@ import * as Twilio from 'twilio';
  * @arg {CognigyScript} `from` Phone number of the sender
  * @arg {CognigyScript} `to` Phone number of the recipient
  * @arg {CognigyScript} `body` SMS Body
- * @arg {Boolean} `writeToContext` Whether to write to Cognigy Context (true) or Input (false)
- * @arg {CognigyScript} `store` Where to store the result
+ * @arg {CognigyScript} `contextStore` Where to store the result
  * @arg {Boolean} `stopOnError` Whether to stop on error or continue
  */
-async function sendSMS(input: IFlowInput, args: { secret: any, body: string, from: string, to: string, writeToContext: boolean, store: string, stopOnError: boolean }): Promise<IFlowInput | {}> {
-	if (!args.secret || !args.secret.accountSid || !args.secret.authToken) return Promise.reject('Secret not present or keys missing (accountSid, authToken)');
+async function sendSMS(input: IFlowInput, args: { secret: any, from: string, to: string, body: string, contextStore: string, stopOnError: boolean }): Promise<IFlowInput | {}> {
+	
+	const { secret, body, from, to , contextStore, stopOnError } = args;
+	const { accountSid, authToken } = secret;
 
-	if (!args.from) return Promise.reject("Sender paramter -from- missing.");
-	if (!args.to) return Promise.reject("Recipient paramter -to- missing.");
-	if (!args.body) return Promise.reject("SMS body missing or empty.");
-	if (args.body.length > 1600) return Promise.reject("SMS body too long (max 1600 characters).");
+	if (!body) throw new Error("SMS body missing or empty.");
+	if (body.length > 1600) throw new Error("SMS body too long (max 1600 characters).");
+	if (!from) throw new Error("The sender is missing. Define the 'from' field.");
+	if (!to) throw new Error("The receiver is missing. Define a 'to' field.");
+	if (!contextStore) throw new Error("The context store key is missing. You have to define where to store the response in the Cognigy Context object.");
+
+	if (!secret) throw new Error("The secret is missing. You have to define the 'accountSid' and 'authToken'.");
+	if (!accountSid) throw new Error("The secret is missing the 'accountSid' field.");
+	if (!authToken) throw new Error("The secret is missing the 'authToken' field.");
 
 	let result = null;
 	try {
-		const client = Twilio(args.secret.accountSid, args.secret.authToken);
+		const client = Twilio(accountSid, authToken);
 
-		result = await client.messages.create({from: args.from, body: args.body, to: args.to});
+		result = await client.messages.create({
+			from,
+			body, 
+			to
+		});
+
 	} catch (err) {
 		if (args.stopOnError) { return Promise.reject(err.message); }
 		result = { "error": err.message};
 	}
 
-	if (args.writeToContext) input.context.getFullContext()[args.store] = result;
-	else input.input[args.store] = result;
+	input.actions.addToContext(contextStore, result, 'simple');
 
 	return input;
 }
 
-// You have to export the function, otherwise it is not available
 module.exports.sendSMS = sendSMS;
