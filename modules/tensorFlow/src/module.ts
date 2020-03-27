91a547a8fa084a4378ed85d27e42b47be5ae0d10
Alexander Teusz <teusz.alexander@gmail.com>
2020-03-27 12:04:08 +0100

    refactored node

--- modules/tensorFlow/src/module.ts
+++ modules/tensorFlow/src/module.ts
@@ -1,23 +1,34 @@
 const npmModel = require('@tensorflow-models/toxicity');
 /**
  * Template for usage of a TensorFlow.js module in a Custom Module
- * @arg {CognigyScript} `store` Where to store the result
- * @arg {Boolean} `writeToContext` Whether to write to Cognigy Context (true) or Input (false)
+ * @arg {CognigyScript} `text` The text that should be classified
+ * @arg {CognigyScript} `contextStore` Where to store the result
+ * @arg {Boolean} `stopOnError` Whether to stop the flow on an error
  */
-async function functionName(input: IFlowInput, args: {store: string, writeToContext: boolean}): Promise<IFlowInput | {}> {
-	// load the model
-	const model = await npmModel.load();
-	// get the sentence from IFlowInput
-	const text = input.input.text;
-	// use the model on the input text as described in the documentation
-	const result = await model.classify(text);
+async function classifyText(input: IFlowInput, args: { text: string, contextStore: string, stopOnError: boolean }): Promise<IFlowInput | {}> {
 
-	return new Promise(resolve => {
-		// write the result to the Context or Input object
-        	if (args.writeToContext) input.context.getFullContext()[args.store] = result;
-	 	else input.input[args.store] = result;
-	        resolve(input);
-    });
+	const { text, contextStore, stopOnError } = args;
+
+	if (!text) throw new Error("The input text is not defined. You can use CognigyScript to access the user input: {{ci.text}}.");
+	if (!contextStore) throw new Error("No context store key is defined. You have to define where to store the node response in the Cogngiy Context.");
+
+	try {
+		// load the model
+		const model = await npmModel.load();
+
+		// use the model on the input text as described in the documentation
+		const result = await model.classify(text);
+
+		input.actions.addToContext(contextStore, result, 'simple');
+	} catch (error) {
+		if (stopOnError) {
+			throw new Error(error.message);
+		} else {
+			input.actions.addToContext(contextStore, { error: error.message }, 'simple');
+		}
+	}
+
+	return input;
 }
-// You have to export the function, otherwise it is not available
-module.exports.functionName = functionName;
+
+module.exports.classifyText = classifyText;
